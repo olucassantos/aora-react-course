5f0ec3917921e57d3b226d12da13417a6bbafb37
Lucas Anjos <oargus.g@gmail.com>
2024-08-12 21:55:22 -0300

    Tela de inicio e componente de botão

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-
 import { Link } from 'expo-router';
+import { Text, View, ScrollView, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import CustomButton from '../components/CustomButton';
+
+import { images } from '../constants';
 
 export default function App() {
     return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl font-pblack">Aora!</Text>
-            <StatusBar style="auto" />
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView contentContainerStyle={{ height: '100%' }}>
+                <View className="w-full justify-center items-center h-full px-4">
+                    <Image 
+                        source={images.logo}
+                        resizeMode='contain'
+                        className="w-[130px] h-[84px]"
+                    />
 
-            <Link href="/home" style={{ color: 'blue' }}>Home</Link>
-        </View>
+                    <Image 
+                        source={images.cards}
+                        className="max-w-[330px] h-[300px] w-full"
+                        resizeMode='contain'
+                    />
+
+                    <View className="relative mt-5">
+                        <Text className="text-4xl text-white font-bold text-center">
+                            Descubra possibilidades infinitas com {' '}
+                            <Text className="text-secondary-200">Aora</Text>
+                        </Text>
+                        
+                        <Image 
+                            source={images.path}
+                            className="absolute w-[136px] h-[15px] -bottom-2 -right-0"
+                            resizeMode='contain'
+                        />
+                    </View>
+
+                    <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+                        Onde a criatividade encontra inovação: embarque em uma jornada infinitas explorações com a Aora.
+                    </Text>
+
+                    <CustomButton 
+                        title="Continuar com e-mail"
+                        handlePress={() => {}}
+                        containerStyles="w-full mt-7"
+                    />
+                </View>
+            </ScrollView>
+
+            <StatusBar backgroundColor='#161622' style='light' />
+        </SafeAreaView>
     );
 }
