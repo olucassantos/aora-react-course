dafc9cee27ba7de950563a184a967129a33de5f4
Lucas Anjos <oargus.g@gmail.com>
2024-08-14 21:20:41 -0300

    feat: Add Expo AV library and integrate video playback in Trending and VideoCard components

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import * as Animatable from 'react-native-animatable';
 
 import { icons } from '../constants'
+import { Video, ResizeMode } from 'expo-av';
 
 const zoomIn = {
     0: {
@@ -32,7 +33,18 @@ const TrendingItem = ({ activeItem, item }) => {
             duration={500}
         >
             {play ? (
-                <Text className="text-white">Playing</Text>
+                <Video 
+                    source={{ uri: item.video, overrideFileExtensionAndroid: 'mp4' }}
+                    className="w-52 h-72 rounded-[35px] mt-3 bg-white/10"
+                    resizeMode={ResizeMode.CONTAIN}
+                    useNativeControls
+                    shouldPlay
+                    onPlaybackStatusUpdate={(status) => {
+                        if (status.didJustFinish) {
+                            setPlay(false);
+                        }
+                    }}
+                />
             ) : (
                 <TouchableOpacity
                     className="relative justify-center items-center"
--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -2,6 +2,7 @@ import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 
 import { icons } from '../constants'
+import { ResizeMode, Video } from 'expo-av'
 
 const VideoCard = ({ video: { title, thumbnail, video, users: { avatar, username }} }) => {
     const [play, setPlay] = useState(false)
@@ -38,7 +39,18 @@ const VideoCard = ({ video: { title, thumbnail, video, users: { avatar, username
             </View>
 
             {play ? (
-                <Text className="text-white">Playing</Text>
+                <Video
+                    source={{ uri: video, overrideFileExtensionAndroid: 'mp4' }}
+                    className="w-full h-60 rounded-xl mt-3"
+                    resizeMode={ResizeMode.CONTAIN}
+                    useNativeControls
+                    shouldPlay
+                    onPlaybackStatusUpdate={(status) => {
+                        if (status.didJustFinish) {
+                            setPlay(false);
+                        }
+                    }}
+                />
             ) : (
                 <TouchableOpacity 
                     className="w-full h-60 rounded-xl mt-3 relative justify-center items-center"
